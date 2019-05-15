9d278f9c14e508ab4ca258e18089b1f143d2fe87
chenmanru75 <chenmanru75@163.com>
2019-05-15 21:40:17 +0800

    ReduceTest getAverage ok

--- src/main/java/com/thoughtworks/collection/Reduce.java
+++ src/main/java/com/thoughtworks/collection/Reduce.java
@@ -33,7 +33,13 @@ public class Reduce {
     }
 
     public double getAverage() {
-        throw new NotImplementedException();
+        double sum = 0;
+        double avg = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            sum = sum + arrayList.get(i);
+        }
+        avg = sum/arrayList.size();
+        return avg;
     }
 
     public double getOrderedMedian() {
