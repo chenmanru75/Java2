7429c2fd6ec7433b51439d94d2660f09fb1a70d5
chenmanru75 <chenmanru75@163.com>
2019-05-15 13:41:34 +0800

    CollectionOperatorTest getListByInterval ok

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,27 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        Integer[] arr = new Integer[Math.abs(right-left)+1];
+        if(left <= right){
+            int leftCopy = left;
+            for (int i = 0; i <= right-left; i++) {
+                arr[i] = leftCopy;
+                leftCopy++;
+            }
+        }else{
+            int Copy = left;
+            for (int j = 0; j <= left-right; j++) {
+                arr[j] = Copy;
+                Copy--;
+            }
+        }
+        List<Integer> resList = Arrays.asList(arr);
+        return resList;
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
